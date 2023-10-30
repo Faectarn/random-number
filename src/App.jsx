639da7047eed632f7287c6235aef07f50c55fda6
Andreas <andreas_factare@hotmail.com>
2023-10-30 11:54:54 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
   const [maxNumber, setMaxNumber] = useState(null);
   const [randomNumber, setRandomNumber] = useState(null);
   const [shuffling, setShuffling] = useState(false);
@@ -21,20 +22,42 @@ function App() {
     }, 50);
   };
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const num = parseInt(inputValue, 10);
+    if (!isNaN(num) && num > 0) {
+      handleNumberSelectAndGenerate(num);
+    }
+  };
+
   return (
     <>
       {/* <h1>Vite + React</h1> */}
       <div className="card">
-        <div className="button-row">
-          {Array.from({ length: 9 }, (_, i) => i + 2).map((num) => (
-            <button 
-              key={num} 
-              onClick={() => handleNumberSelectAndGenerate(num)} 
-              disabled={shuffling} 
+        <div className="button-grid">
+          {Array.from({ length: 8 }, (_, i) => i + 2).map((num) => (
+            <button
+              key={num}
+              onClick={() => handleNumberSelectAndGenerate(num)}
+              disabled={shuffling}
               className={num === maxNumber ? 'selected' : ''}>
               {num}
             </button>
           ))}
+        </div>
+        <div className="input-section">
+          <input
+            className='number-input'
+            type="number"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="10"
+            disabled={shuffling}
+          />
+          <button className="submit-button" onClick={handleSubmit} disabled={shuffling}>Shuffle</button>
         </div>
         <div className="number-row">
           {maxNumber && Array.from({ length: maxNumber }, (_, i) => i + 1).map((num) => (
